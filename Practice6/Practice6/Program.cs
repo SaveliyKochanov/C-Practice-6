da311af2b3777e5195420472502bc852ea44913d
SaveliyKochanov <122664973+SaveliyKochanov@users.noreply.github.com>
2023-05-20 17:40:53 +0700

    Add files via upload

---
+++ Practice6/Practice6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace practicework5
+{
+    class programm
+    {
+        public static void Main(string[] args)
+        {
+          
+            int a = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            int c = 1;
+            while (k != n)
+            {
+                k = k + 1;
+                c = Convert.ToInt32(Math.Pow(c, a));
+                Console.WriteLine(c);
+            }
+           
+
+
+
+        }
+    }
+
+
+}
+
+
